39caa32443c94b1e4abc729ccfd218c3cc02534e
louis nwosu <nwachukwuchinanu1@gmail.com>
2021-04-03 22:08:36 +0000

    sick

--- noted-v2/src/components/navbar.jsx
+++ noted-v2/src/components/navbar.jsx
@@ -1,35 +1,41 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import "../hover.css";
 import "../App.css";
 
 const NavbarComp = () => {
   return (
-    <Navbar fixed="top" className="navbar-sec">
-      <Navbar.Brand href="#home" className="text-light mr-auto">
-        Jott3d
-      </Navbar.Brand>
-      <Nav>
-        <Nav.Link
-          href="#home"
-          className="text-light mx-2 hvr-underline-from-right"
-        >
-          Home
-        </Nav.Link>
-        <Nav.Link
-          href="#features"
-          className="text-light mx-2 hvr-underline-from-right"
-        >
-          App
-        </Nav.Link>
-        <Nav.Link
-          href="#pricing"
-          className="text-light mx-2 hvr-underline-from-right"
-        >
-          developer
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+    <Container>
+      <Row>
+        <Col>
+          <Navbar fixed="top" className="navbar-sec py-4 px-3">
+            <Navbar.Brand href="#home" className="text-dark mr-auto font-weight-bold">
+              Jott3d
+            </Navbar.Brand>
+            <Nav>
+              <Nav.Link
+                href="#home"
+                className="text-dark mx-2 hvr-underline-from-right"
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#features"
+                className="text-dark mx-2 hvr-underline-from-right"
+              >
+                App
+              </Nav.Link>
+              <Nav.Link
+                href="#pricing"
+                className="text-dark mx-2 hvr-underline-from-right"
+              >
+                developer
+              </Nav.Link>
+            </Nav>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
