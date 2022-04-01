8e2d9f312adeceba88950f9c577abdc2456e3d6e
L3000 <nwachukwuchinanu1@gmail.com>
2022-04-01 03:28:37 +0100

    added modal for password

--- web-client/src/layouts/applayout/appLayout.tsx
+++ web-client/src/layouts/applayout/appLayout.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useState, Fragment } from "react";
 
-import Grid from "@mui/material/Grid";
+import { Grid } from "@mui/material";
 import { makeStyles, createStyles } from "@mui/styles";
 import { Theme } from "@mui/system";
 
@@ -20,6 +20,35 @@ const useStyles = makeStyles((theme: Theme) =>
         display: "none",
       },
     },
+    dialogContainer: {
+      position: 'absolute',
+      width: '100%',
+      height: '100vh',
+      top: '0',
+      // display: 'none'
+    },
+    backDrop: {
+      opacity: '0.7',
+      backgroundColor: '#000',
+      width: '100%',
+      height: '100%',
+    },
+    dialogItem: {
+      position: 'absolute',
+      width: '20%',
+      backgroundColor: '#fff',
+      padding: '20px 5px',
+      top: '40vh',
+      left: '45%'
+    },
+    container: {
+      position: "relative",
+    },
+    dialogIns: {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+    }
   })
 );
 
@@ -28,14 +57,22 @@ export const AppLayout: FC = ({ children }) => {
   const [modalState, setModalState] = useState<boolean>(false);
   const handleModalState = () => setModalState(true);
   return (
-    <Grid container>
-      <Grid item md={2} xs={12} className={classes.sideNav}>
-        <SideNav setModalState={handleModalState} />
+    <Fragment>
+      <Grid container className={classes.container}>
+        <Grid item md={2} xs={12} className={classes.sideNav}>
+          <SideNav setModalState={handleModalState} />
+        </Grid>
+        <Grid item md={10} xs={12} style={{ marginLeft: "auto" }}>
+          {children}
+        </Grid>
+        <div className={classes.dialogContainer}>
+          <div className={classes.dialogIns}>
+            <div className={classes.backDrop}></div>
+            <div className={classes.dialogItem}></div>
+          </div>
+        </div>
       </Grid>
-      <Grid item md={10} xs={12} style={{ marginLeft: "auto" }}>
-        {children}
-      </Grid>
-    </Grid>
+    </Fragment>
   );
 };
 
